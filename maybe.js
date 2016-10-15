964a651ca22e5c0c829156f9520de7991c229a5e
Chessboard <chessboardthedev@users.noreply.github.com>
2016-10-15 02:24:15 -0400

    Create maybe.js

---
+++ maybe.js
@@ -0,0 +1,12 @@
+function load() {
+	setTimeout(function() {
+		$('span.loading').fadeOut('medium').fadeIn('medium');
+		load();
+	}, 0);
+}
+
+$(document).ready(function() {
+setTimeout(function() {
+challenge();
+}, 5000);
+});
